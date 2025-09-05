d2a3245a6fc2955727fcaa1caf15eea957bc7b02
sho-pb <shota.tagawa@plan-b.co.jp>
2025-09-05 11:55:45 +0900

    update: postIdをrandomにする

--- apps/web-a/app/_components/Post.tsx
+++ apps/web-a/app/_components/Post.tsx
@@ -1,6 +1,8 @@
 export const Post = async () => {
+  const postId = Math.floor(Math.random() * 10) + 1;
+
   const data = (await fetch(
-    'https://jsonplaceholder.typicode.com/posts/1'
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
   ).then((response) => response.json())) as
     | {
         id: number;
