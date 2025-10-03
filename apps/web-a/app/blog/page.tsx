6fb1f28114f448b456f7394e0884e28274549648
sho-pb <shota.tagawa@plan-b.co.jp>
2025-10-03 11:41:35 +0900

    update: 😄

---
+++ apps/web-a/app/blog/page.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return <div style={{
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '32px',
+    fontWeight: 'bold',
+  }}>😄😄😄 This is the blog page 😄😄😄</div>
+}
+
+export default Page;
