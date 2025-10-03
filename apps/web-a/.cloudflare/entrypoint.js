c18f8ad7b346ef248d775a3d9a197496d904ebb2
sho-pb <shota.tagawa@plan-b.co.jp>
2025-10-03 11:46:55 +0900

    update: 😄

--- apps/web-a/.cloudflare/entrypoint.js
+++ apps/web-a/.cloudflare/entrypoint.js
@@ -1,21 +1,29 @@
-import openNextWorker from "../.open-next/worker.js";
+import openNextWorker from '../.open-next/worker.js';
 
 // eslint-disable-next-line
 export default {
   async fetch(request, env, ctx) {
-    const auth = request.headers.get("Authorization");
-    const valid = "Basic " + btoa(`admin:password`);
+    const auth = request.headers.get('Authorization');
+    const valid = 'Basic ' + btoa(`admin:password`);
+
+    let _request = request;
+
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/articles')) {
+      url.pathname = url.pathname.replace('/articles', '/blog');
+      _request = new Request(url.toString(), request);
+    }
 
     if (auth !== valid) {
-      return new Response("Unauthorized", {
+      return new Response('Unauthorized', {
         status: 401,
         headers: {
-          "WWW-Authenticate": 'Basic realm="Restricted"',
+          'WWW-Authenticate': 'Basic realm="Restricted"',
         },
       });
     }
 
     // 認証OKなら OpenNext の worker に渡す
-    return openNextWorker.fetch(request, env, ctx);
+    return openNextWorker.fetch(_request, env, ctx);
   },
 };
