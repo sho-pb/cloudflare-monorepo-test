7ce55d2ac00b9582f91bfd81e906a62aca99b44d
sho-pb <shota.tagawa@plan-b.co.jp>
2025-10-03 11:50:43 +0900

    update: immutableにする

--- apps/web-a/.cloudflare/entrypoint.js
+++ apps/web-a/.cloudflare/entrypoint.js
@@ -1,18 +1,24 @@
 import openNextWorker from '../.open-next/worker.js';
 
+const generateCustomRequest = (request) => {
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith('/articles')) {
+    url.pathname = url.pathname.replace('/articles', '/blog');
+
+    return new Request(url.toString(), request);
+  }
+
+  return request;
+};
+
 // eslint-disable-next-line
 export default {
   async fetch(request, env, ctx) {
     const auth = request.headers.get('Authorization');
     const valid = 'Basic ' + btoa(`admin:password`);
 
-    let _request = request;
-
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/articles')) {
-      url.pathname = url.pathname.replace('/articles', '/blog');
-      _request = new Request(url.toString(), request);
-    }
+    const customRequest = generateCustomRequest(request);
 
     if (auth !== valid) {
       return new Response('Unauthorized', {
@@ -24,6 +30,6 @@ export default {
     }
 
     // 認証OKなら OpenNext の worker に渡す
-    return openNextWorker.fetch(_request, env, ctx);
+    return openNextWorker.fetch(customRequest, env, ctx);
   },
 };
