ef9e3611a554ab23f9f61095c6ae6bd0b6c33320
sho-pb <shota.tagawa@plan-b.co.jp>
2025-10-10 11:35:55 +0900

    update: サーバーサイド専用の環境変数を参照する

--- apps/web-a/app/api/status/route.ts
+++ apps/web-a/app/api/status/route.ts
@@ -3,5 +3,7 @@ export const dynamic = 'force-dynamic';
 export async function GET() {
   console.log('logging from api/status/route.ts');
 
-  return Response.json({ status: process.env.NEXT_PUBLIC_TEST || '悲報' });
+  return Response.json({
+    status: process.env.TEST || process.env.NEXT_PUBLIC_TEST || '悲報',
+  });
 }
